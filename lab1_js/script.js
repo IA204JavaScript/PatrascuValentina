86d32e7802b5fa99bdfe2c77168696eb4cce178d
Valentina Patrascu <valentina12andreevna1@gmail.com>
2025-02-12 00:33:38 +0200

    Add files via upload

---
+++ lab1_js/script.js
@@ -0,0 +1,18 @@
+alert("this code is executed from an external file ");
+console.log("message in the console");
+let name = "Valentina";
+let age = 20;
+let isStudent = true;
+let score = prompt("write your score:");
+if (score >= 90) {
+    console.log("great!");
+} else if (score >= 70) {
+    console.log("good");
+} else {
+    console.log("can be better!");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(`interation: ${i}`);
+}
+
